31eb762b2ad25bff9460704129e0a6c5efe2a6be
Jens Papenhagen <jens.papenhagen@web.de>
2025-11-25 20:44:47 +0100

    adding config

---
+++ src/main/java/de/papenhagen/weatherapi/config/BaseConfig.java
@@ -0,0 +1,26 @@
+package de.papenhagen.weatherapi.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tools.jackson.core.StreamReadFeature;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.TimeZone;
+
+@Configuration
+public class BaseConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .changeDefaultPropertyInclusion(incl -> incl.withValueInclusion(JsonInclude.Include.ALWAYS))
+                .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES) //not really care if all fields are filled
+                // .disable(MapperFeature.DEFAULT_VIEW_INCLUSION) in Jackson 3 this is default disabled
+                // .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES) in Jackson 3 this is default disabled
+                .defaultTimeZone(TimeZone.getTimeZone("UTC")) // set a default timezone for dates
+                .build();
+    }
+}
