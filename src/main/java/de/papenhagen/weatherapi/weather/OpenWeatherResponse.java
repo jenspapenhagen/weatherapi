a897b274cdd541e8a17a903fe8bbcf3512c908fd
Jens Papenhagen <jens.papenhagen@web.de>
2025-11-25 20:45:19 +0100

    move class

--- src/main/java/de/papenhagen/weatherapi/weather/OpenWeatherResponse.java
+++
@@ -1,7 +0,0 @@
-package de.papenhagen.weatherapi.weather;
-
-public record OpenWeatherResponse(Main main, Wind wind, Weather[] weather) {
-    public record Main(double temp, double humidity) {}
-    public record Wind(double speed) {}
-    public record Weather(String description) {}
-}
