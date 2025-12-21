2b2d60c3d001b72ae2ae76970d8bdc6e6e559e0e
Jens Papenhagen <jens.papenhagen@web.de>
2025-12-21 15:33:10 +0100

    move spring ai version up because spring boot 4 problems but context still not loading, will check it later

--- src/test/java/de/papenhagen/weatherapi/WeatherapiApplicationTests.java
+++ src/test/java/de/papenhagen/weatherapi/WeatherapiApplicationTests.java
@@ -1,12 +1,9 @@
 package de.papenhagen.weatherapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.restclient.autoconfigure.RestClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {
-        RestClientAutoConfiguration.class
-})
+@SpringBootTest
 class WeatherapiApplicationTests {
 
 	@Test
